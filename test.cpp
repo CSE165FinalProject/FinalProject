b56e6706b698a847bb79d6ceadb11d3f3addbb7c
simular124 <91218756+simular124@users.noreply.github.com>
2021-11-07 14:09:22 -0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -8,7 +8,10 @@ using namespace std;
 void sy();
 void ralph();
 void vedaant();
-
+void ralph()
+{
+    cout << "Ralph is here" << endl;
+}
 int main(){
 
     sy(); //implement a function of your choice
