d7b09dc4506cda7e254abef4de369a4feea0b9ca
tylller101 <79801694+tylller101@users.noreply.github.com>
2021-11-07 14:04:38 -0800

    added testing cpp and makefile

---
+++ test.cpp
@@ -0,0 +1,18 @@
+//Tyler
+// Nov 7, 2021
+
+#include <iostream>
+
+using namespace std;
+
+void sy();
+void ralph();
+void vedaant();
+
+int main(){
+
+    sy(); //implement a function of your choice
+    ralph(); //implement a function of your choice
+    vedaant(); //implement a function of your choice
+
+}
