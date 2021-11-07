85508958aa06df609678815f67b2288ec471ce6f
tylller101 <79801694+tylller101@users.noreply.github.com>
2021-11-07 14:55:20 -0800

    reset

--- test.cpp
+++ test.cpp
@@ -7,23 +7,12 @@ using namespace std;
 
 void sy();
 void ralph();
-<<<<<<< HEAD
 void vedaant();
 
-=======
-void vedaant(int a, int b, int c){
-    int sum = a+b+c;
-    cout<<sum;
-};
-void ralph()
-{
-    cout << "Ralph is here" << endl;
-}
->>>>>>> 25fcada7180949bc8825efee5db476d297fbc216
 int main(){
 
     sy(); //implement a function of your choice
     ralph(); //implement a function of your choice
-    vedaant(4,54,23); //implement a function of your choice
+    vedaant(); //implement a function of your choice
 
 }
