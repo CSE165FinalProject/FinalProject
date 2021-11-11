7bff6d239b756bf7887f8c2628e2c3880f3236c5
lalakerspro <vvyas@ucmerced.edu>
2021-11-11 01:54:13 -0800

    original

---
+++ test.cpp
@@ -0,0 +1,28 @@
+//Tyler
+// Nov 7, 2021
+
+#include <iostream>
+
+using namespace std;
+
+void sy();
+void ralph();
+void vedaant();
+
+int main(){
+
+    sy(); //implement a function of your choice
+    ralph(); //implement a function of your choice
+    vedaant(); //implement a function of your choice
+
+}
+
+void vedaant(){
+
+
+    cout<<"This professor sucks a**. ";
+}
+void ralph()
+{
+    cout << "Ralph here" << endl;
+}
