438fd1501199ea21e93332939b6e52a2ebdafb88
tylller101 <79801694+tylller101@users.noreply.github.com>
2021-12-02 17:05:12 -0800

    changes so i can pull ralphs code

---
+++ detection.h
@@ -0,0 +1,85 @@
+#include "objects/character.h"
+#include "objects/cloud.h"
+#include "death.cpp"
+#include "objects/powerup.h";
+//#include "oHNO3.cpp"
+extern int gameover;
+extern int life;
+extern int fullHEALTH;
+void detectingCLOUD(BadCloud&BC, character& ch)
+{
+	int cloudPOSX, cloudPOSY;
+	cloudPOSX = BC.getXPOS();
+	cloudPOSY = BC.getYPOS();
+	//int gameover;
+	int posPlayerX = ch.getXPOSC();
+	int posPlayerY = ch.getYPOSC();
+	//printf("%d %d\n", cloudPOSX, cloudPOSY);
+	//printf("%d", cloudPOSY);
+	if (BC.attackIND == 2)
+	{
+
+
+		if ((cloudPOSX < posPlayerX && cloudPOSX + BC.CLOUD_WIDTH > posPlayerX&& posPlayerY > cloudPOSY + BC.CLOUD_HEIGHT-100))
+		{
+
+			//gameover = true;
+			if (life > 0)
+			{
+				life--;
+			}
+			else
+			{
+				gameover = true;
+			}
+			//gameover = true;
+			 //call the death cpp
+			//printf("%d", gameover);
+			//death();
+		}
+		else
+		{
+			gameover = false;
+			//printf("%d", gameover);
+		}
+	}
+	//if (cloudPOS < posPlayer && cloudPOS + BC.CLOUD_WIDTH > posPlayer)
+	//{
+	//	gameover = true; //call the death cpp
+	//	printf("%d", gameover);
+	//	//death();
+	//}
+	//else
+	//{
+	//	gameover = false;
+	//	//printf("%d", gameover);
+	//}
+}
+void detectionPOWERUP(powerup&pu, character&player)
+{
+	int xpospu = pu.getXPOS();
+	int ypospu = pu.getYPOS();
+	int xpospl = player.getXPOSC();
+	//printf(" %d %d\n", xpospu, ypospu);
+	int ypospl = player.getYPOSC();
+	//printf(" %d %d |  %d %d\n", xpospl, ypospl, xpospu,ypospu);
+
+	if ((ypospl+20 > ypospu && ypospl+20 < ypospu + pu.POWERUP_HEIGHT) && (xpospl + 20 > xpospu && xpospl + 20 < xpospu + pu.POWERUP_WIDTH))
+	{
+			if (life < 20)
+			{
+				life++;
+			}
+			else
+			{
+				fullHEALTH = 1;
+			}
+	}
+	else
+	{
+		fullHEALTH = 0;
+	}
+
+
+
+} 
---
+++ objects/powerup.h
@@ -0,0 +1,85 @@
+#ifndef powerup_H
+#define powerup_H
+
+//CSE165 oHNO3 game
+//Tyler Armstrong, Ralphilou Tatoy, add names guys
+//Nov 17, 2021
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <time.h>
+//#include "sdlcreatewindow.cpp"
+//#include <createSDLWIN.h>
+//#include "imageloader.cpp"
+extern int timer,timer1, timer2, timer3, timer4;
+extern int timerStarted1, timerStarted2, timerStarted3, timerStarted4;
+
+//#include "oHNO3.cpp"
+class powerup
+{
+    public:
+        //The dimensions of the Power up
+        static const int POWERUP_WIDTH = 100;
+        static const int POWERUP_HEIGHT = 100;
+
+        //Maximum axis velocity of the Power up
+        static const int CLOUD_VEL = 6;
+        //Motion of the cloud left to right and vice version
+        //void movementBADCLOUD();
+        //Initializes the variables
+        powerup();
+
+        //Shows the Power up on the screen
+       // void render();
+
+		//friend character;
+		//Get X Position of the powerup
+		int getXPOS();
+		//Get Y position of the powerup
+		int getYPOS();
+
+        //Spawn Powerup
+        void spawn();
+        //Render powerup
+        void render();
+		//int getYPOS();
+    private:
+        //The X and Y offsets of the cloud
+        int mPosX, mPosY;
+
+
+		//int id;
+		//int numAttack;
+        //The velocity of the Cloud
+        int mVelX, mVelY;
+};
+powerup::powerup()
+{
+    //Initialize coordinates and velocity
+    mPosX = 0;
+    mPosY = 0;
+    mVelX = 0;
+    mVelY = 0;
+}
+int powerup::getYPOS()
+{
+    //Get X position of object
+	return mPosY;
+}
+int powerup::getXPOS()
+{
+    //Get Y position of object
+    return mPosX;
+}
+
+void powerup::spawn()
+{
+    //Get random coordinates;
+    srand(time(NULL));
+    int xspawncoords = rand() % 800 + 1;
+    int yspawncoords = rand() % 400 + 1;
+    mPosX = xspawncoords;
+    mPosY = yspawncoords;
+
+}
+#endif
